d8819b375251b91858d4314bc6f0a86ae53595b9
stevie <stevievines@gmail.com>
2013-03-14 11:00:11 -0400

    Create Ruby App (RSpec Framework)

---
+++ dive_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiveApp::Application.config.secret_token = 'bc710e5a8c0f6da01f793ffb14a9ab6e7baca328e7536973fada8d1b02285d29c670224a275f1e610ccdffadaaf06ee4b54351359f36ddafecd1e8185d0226eb'
