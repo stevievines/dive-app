cb8deab857cf512ff73cef2517303f7f2fd83d9b
stevie <stevievines@gmail.com>
2013-04-17 11:07:50 -0400

    Finish initial static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+	let(:base_title) { "Diving App" }
+
+	describe "Home page" do
+
+		it "should have the h1 'Diving App' " do
+			visit '/static_pages/home'
+			page.should have_selector('h1', :text => 'Diving App')
+		end
+
+		it "should have the title 'Home' " do
+			visit '/static_pages/home'
+			page.should have_selector('title',
+								:text => "#{base_title} | Home")
+		end
+	end
+
+	describe "About page" do
+
+		it "should have the h1 'About Us' " do
+			visit '/static_pages/about'
+			page.should have_selector('h1', :text => 'About Us')
+		end
+
+		it "should have the title 'About Us' " do
+			visit '/static_pages/about'
+			page.should have_selector('title',
+								:text => "#{base_title} | About Us")
+		end
+	end
+
+	describe "Contact page" do
+
+		it "should have the h1 'Contact' " do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact')
+		end
+
+		it "should have the title 'Contact' " do
+			visit '/static_pages/contact'
+			page.should have_selector('title',
+								:text => "#{base_title} | Contact")
+		end
+	end
+
+end
