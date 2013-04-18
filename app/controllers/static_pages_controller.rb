31c5c3b5cb3f610a34059ad745fb0dfa36137aa5
stevie <stevievines@gmail.com>
2013-04-17 23:36:25 -0400

    Make a basic User model (with secure passwords)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,10 @@
 class StaticPagesController < ApplicationController
-	def home
-	end
+  def home
+  end
 
-	def about
-	end
+  def about
+  end
 
-	def contact
-	end
+  def contact
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,27 +2,27 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  	subject { page }
+  subject { page }
 
- 	describe "Home page" do
-	    before { visit root_path } 
+  describe "Home page" do
+    before { visit root_path } 
 
-	    it { should have_selector('h1', text: 'Diving App') }
-	    it { should have_selector('title', text: full_title('')) }
-	    it { should_not have_selector 'title', text: '| Home' }
-	end
+    it { should have_selector('h1', text: 'Diving App') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
+  end
 
-	describe "About page" do
-		before { visit about_path }
+  describe "About page" do
+    before { visit about_path }
 
-	    it { should have_selector('h1',    text: 'About') }
-	    it { should have_selector('title', text: full_title('About Us')) }
-	end
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About Us')) }
+  end
 
-	describe "Contact page" do
-    	before { visit contact_path }
+  describe "Contact page" do
+    before { visit contact_path }
 
-	    it { should have_selector('h1',    text: 'Contact') }
-	    it { should have_selector('title', text: full_title('Contact')) }
-	end
+    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
+  end
 end
